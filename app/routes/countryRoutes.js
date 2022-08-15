799c65c9acc9bf240a6c86789e83b5badfeed01f
John Kennedy Kalu <72397673+kvngdre@users.noreply.github.com>
2022-08-15 08:22:41 +0100

    removed route to get a single country

--- app/routes/countryRoutes.js
+++ app/routes/countryRoutes.js
@@ -9,12 +9,4 @@ router.get('/', async (req, res) => {
     return res.status(200).send(response);
 });
 
-router.get('/:id', async (req, res) => {
-    const response = await countryCtrlFuncs.getOne(req.params.id);
-    if (response instanceof Error)
-        return res.status(404).send(response.message);
-
-    return res.status(200).send(response);
-});
-
 module.exports = router;
